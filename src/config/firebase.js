10c7f887568b62b5008d693e1c86d566e4234598
AdminForIinRage <bhattaraianjesh123@gmail.com>
2024-01-10 12:40:46 +1030

    firebase and accessing realtime

---
+++ src/config/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import {getFirestore} from 'firebase/firestore'
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBWDD28fd5JqULc2HwDHNhcsYnMc5ZYXi0",
+    authDomain: "booklist-af099.firebaseapp.com",
+    projectId: "booklist-af099",
+    storageBucket: "booklist-af099.appspot.com",
+    messagingSenderId: "613454526038",
+    appId: "1:613454526038:web:b236fd2846aace50722df7"
+  };
+  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app) 
+export const googleProvider = new GoogleAuthProvider()
+export const db = getFirestore(app)
+export const storage = getStorage(app);
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import BookList from '../components/BookList'
-import BookForm from '../components/BookForm'
+import { useEffect, useState } from "react";
+import BookList from "../components/BookList";
+import BookForm from "../components/BookForm";
+import { useCollection } from "../hooks/useCollection";
+
+
+
 
 export default function Home() {
-  const [books, setBooks] = useState([
-    { title: 'the name of the wind', id: 1 },
-    { title: 'the dragon reborn', id: 2 },
-    { title: 'the final empire', id: 3 },
-    { title: 'the way of kings', id: 4 }
-  ])
+
+    const {documents: books} = useCollection('books')
+  
+
+
 
   return (
     <div className="App">
@@ -17,3 +20,17 @@ export default function Home() {
     </div>
   );
 }
+
+// import { db } from "../config/firebase";
+// import { collection, doc, getDocs } from "firebase/firestore";
+
+  // useEffect(() => {
+  //   const ref = collection(db, "books");
+  //   getDocs(ref).then((snapshot) => {
+  //     let results = []
+  //     snapshot.docs.forEach(doc => {
+  //       results.push({id: doc.id, ...doc.data()})
+  //     });
+  //     setBooks(results)
+  //   })
+  // }, [books]);
